eb22faffe94e87746a14ed964f0729b01bb5e24e
1e16miin <ddogi1513@naver.com>
2022-03-08 17:25:04 +0900

    feat add sample data

---
+++ src/movie-ticketing-system/data.ts
@@ -0,0 +1,50 @@
+import { Movie } from "./Movie";
+import { Duration, DayOfWeek, LocalTime } from "js-joda";
+import { Money } from "./Money";
+import { AmountDiscountPolicy } from "./AmountDiscountPolicy";
+import { SequenceCondition } from "./SequenceCondition";
+import { PeriodCondition } from "./PeriodCondition";
+import { PercentDiscountPolicy } from "./PercentDiscountPolicy";
+
+const avatar: Movie = new Movie(
+	"아바타",
+	Duration.ofMinutes(120),
+	Money.wons(10000),
+	new AmountDiscountPolicy(
+		Money.wons(800),
+		new SequenceCondition(1),
+		new SequenceCondition(10),
+		new PeriodCondition(
+			DayOfWeek.MONDAY,
+			LocalTime.of(10, 0),
+			LocalTime.of(11, 59)
+		),
+		new PeriodCondition(
+			DayOfWeek.THURSDAY,
+			LocalTime.of(10, 0),
+			LocalTime.of(11, 59)
+		)
+	)
+);
+
+const titanic: Movie = new Movie(
+	"타이타닉",
+	Duration.ofMinutes(189),
+	Money.wons(11000),
+	new PercentDiscountPolicy(
+		0.1,
+		new PeriodCondition(
+			DayOfWeek.TUESDAY,
+			LocalTime.of(14, 0),
+			LocalTime.of(15, 59)
+		),
+		new SequenceCondition(2),
+		new PeriodCondition(
+			DayOfWeek.THURSDAY,
+			LocalTime.of(10, 0),
+			LocalTime.of(13, 59)
+		)
+	)
+);
+
+export { avatar, titanic };
